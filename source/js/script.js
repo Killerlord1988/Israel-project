f7eb9af16d6b889848893227014fe202847e6760
Никита <killerlord1988@gmail.com>
2020-09-28 00:23:19 +0400

    add preview-inst

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
 'use strict';
-// var pageHeader = document.querySelector('.page-header');
-// var headerToggle = document.querySelector('.page-header__toggle');
-
-// pageHeader.classList.remove('page-header--nojs');
-
-// headerToggle.addEventListener('click', function () {
-//   if (pageHeader.classList.contains('page-header--closed')) {
-//     pageHeader.classList.remove('page-header--closed');
-//     pageHeader.classList.add('page-header--opened');
-//   } else {
-//     pageHeader.classList.add('page-header--closed');
-//     pageHeader.classList.remove('page-header--opened');
-//   }
-// });
+$(window).on('load resize', function () {
+  if ($(window).width() < 767) {
+    $('.about-life__slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 100,
+      slidesToShow: 1,
+      // autoplay: true,
+      appendArrows: false,
+    });
+  } else {
+    $('.about-life__slider').slick('unslick');
+  }
+});
