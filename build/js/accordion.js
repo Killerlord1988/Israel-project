33e8ad23fc3985f0fc54255a42b9d9bc7a88679e
Никита <killerlord1988@gmail.com>
2020-09-30 15:10:14 +0400

    add hover and active on button

--- build/js/accordion.js
+++ build/js/accordion.js
@@ -1,9 +1,8 @@
 'use strict';
 var accordion = document.querySelectorAll('.faq__item');
-var answer = document.querySelectorAll('.faq__answer');
 
 for (var i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener('click', function() {
-    this.classList.toggle('faq__answer--active')
-  })
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('faq__answer--active');
+  });
 }
