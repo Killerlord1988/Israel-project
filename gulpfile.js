a0211e267a3b6c19b5f4c47a41ef6ccdef78d044
Никита <killerlord1988@gmail.com>
2020-10-06 19:52:07 +0400

    optimizate

--- gulpfile.js
+++ gulpfile.js
@@ -107,5 +107,5 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "script-min", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "css", "images", "script-min", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
