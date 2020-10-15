19a24fec8d4d5bb133a17ecdea5163d16658bbf7
Никита <killerlord1988@gmail.com>
2020-10-15 13:25:46 +0400

    fix program tabs focus

--- source/js/tabs.js
+++ source/js/tabs.js
@@ -2,25 +2,59 @@ var programsBtn = document.querySelectorAll('.programs__btn');
 var programsInfo = document.querySelectorAll('.programs__info-block');
 var programsLabel = document.querySelectorAll('.programs__label');
 
-programsInfo[0].style.opacity = 1;
-programsInfo[0].style.display = 'block';
+var styles = {
+  active: {
+    opacity: 1,
+    display: 'block',
+  },
+  inactive: {
+    opacity: 0,
+    display: 'none',
+  },
+}
 
-function hiddenProgramsInfo() {
+var STATE = {
+  ACTIVE: 'active',
+  INACTIVE: 'inactive',
+}
+
+function toggleInfoState(info, state) {
+  info.style.opacity = styles[state].opacity;
+  info.style.display = styles[state].display;
+}
+
+toggleInfoState(programsInfo[0], STATE.ACTIVE)
+
+function hideProgramsInfo() {
   Object.keys(programsInfo).forEach(function (el) {
-    programsInfo[el].style.opacity = 0;
-    programsInfo[el].style.display = 'none';
+    toggleInfoState(programsInfo[el], STATE.INACTIVE)
     programsLabel[el].classList.remove('programs__label--checked')
-
   })
 };
 
+function showProgramsInfo(el) {
+  if (programsBtn[el].checked) {
+    toggleInfoState(programsInfo[el], STATE.ACTIVE)
+    programsLabel[el].classList.add('programs__label--checked')
+  }
+}
+
 Object.keys(programsBtn).forEach(function (el) {
   programsBtn[el].addEventListener('change', function () {
-    hiddenProgramsInfo();
-    if (programsBtn[el].checked) {
-      programsInfo[el].style.opacity = 1;
-      programsInfo[el].style.display = 'block';
-      programsLabel[el].classList.add('programs__label--checked')
+      hideProgramsInfo();
+      showProgramsInfo(el);
+    })
+});
+
+Object.keys(programsLabel).forEach(function (el) {
+  programsLabel[el].addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+      hideProgramsInfo();
+      programsBtn[el].checked = 'checked';
+      if (programsBtn[el].checked) {
+        programsLabel[el].classList.add('programs__label--checked')
+      }
+      showProgramsInfo(el);
     }
   })
 });
